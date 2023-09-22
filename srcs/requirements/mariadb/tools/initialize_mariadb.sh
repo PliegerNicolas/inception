1d73e677d6c421a7a2c45b96d35d381c087c03a0
nplieger <plieger05@gmail.com>
2023-09-22 18:12:25 +0200

    Working on MariaDB but DB_TITLE isn't recognizedwq

--- srcs/requirements/mariadb/tools/initialize_mariadb.sh
+++ srcs/requirements/mariadb/tools/initialize_mariadb.sh
@@ -1,45 +1,54 @@
 #!/bin/sh
 
+echo "${DB_TITLE}"
+
 # Set run-time folder for mariadb
 if [ -d "/run/mysql" ]; then
 	echo "[i] mysqld already present, skipping creation"
+	chown -R mysql:mysql /run/mysqld
 else
-	echo "[i] musqld not found, creating..."
+	echo "[i] mysqld not found, creating..."
 	mkdir -p /run/mysqld
+	chown -R mysql:mysql /run/mysqld
 fi
-chown -R mysql:mysql /run/mysqld
 
 # Initialize database
-mysql_install_db --user=mysql --ldata=/var/lib/mysql
+mysql_install_db --user=mysql --ldata=/var/lib/mysql >> /dev/null
 
 # Create temporary file to intialize database content
-temp_file="/tmp/my_temp_file.$(data +%s)"
+temp_file="/tmp/my_temp_file.$(date +%s)"
+touch "${temp_file}"
+
 if [ ! -f "$temp_file" ]; then
 	return 1
 fi
 
 cat << EOF > $temp_file
+
 USE mysql;
 FLUSH PRIVILEGES ;
-GRANT ALL ON *.* TO 'root'@'%' identified by '${DB_ROOT_PASSWORD}' WITH GRANT OPTION ;
 GRANT ALL ON *.* TO 'root'@'localhost' identified by '${DB_ROOT_PASSWORD}' WITH GRANT OPTION ;
 SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${DB_ROOT_PASSWORD}') ;
 DROP DATABASE IF EXISTS test ;
 FLUSH PRIVILEGES ;
+
 EOF
 
-if [ -n "${DB_TITLE}" ]; then
+if [ "${DB_TITLE}" != "" ]; then
 
+	echo "[i] AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
 	echo "[i] Creating data base ${DB_TITLE}..."
 	echo "[i] with character set: 'utf8' and collation: 'utf8_general_ci'"
+
+	# Create database
 	echo "CREATE DATABASE IF NOT EXISTS \`${DB_TITLE}\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $temp_file
 
 	# Create admin if not exist
 	if [ -n "${DB_ADMIN_NAME}" ]; then
 		if ! mysql -u root -e "SELECT 1 FROM mysql.user WHERE user = '${DB_ADMIN_NAME}'" | grep -q 1; then
 			echo "[i] Creating user: ${DB_ADMIN_NAME}..."
-			echo "CREATE USER '${DB_ADMIN_NAME}'@'%' IDENTIFIED BY '${DB_ADMIN_PASSWORD}';" >> $temp_file
-			echo "GRANT ALL ON \`${DB_TITLE}\`.* to '${DB_ADMIN_NAME}'@'%';" >> $temp_file
+			echo "CREATE USER '${DB_ADMIN_NAME}'@'localhost' IDENTIFIED BY '${DB_ADMIN_PASSWORD}';" >> $temp_file
+			echo "GRANT ALL ON \`${DB_TITLE}\`.* to '${DB_ADMIN_NAME}'@'localhost';" >> $temp_file
 		else
 			echo "[i] User '${DB_ADMIN_NAME}' already exists in database."
 		fi
@@ -49,14 +58,13 @@ if [ -n "${DB_TITLE}" ]; then
 	if [ -n "${DB_USER_NAME}" ]; then
 		if ! mysql -u root -e "SELECT 1 FROM mysql.user WHERE user = '${DB_USER_NAME}'" | grep -q 1; then
 			echo "[i] Creating user: ${DB_USER_NAME}..."
-			echo "CREATE USER '${DB_USER_NAME}'@'%' IDENTIFIED BY '${DB_USER_PASSWORD}';" >> $temp_file
-			echo "GRANT ALL ON \`${DB_TITLE}\`.* to '${DB_USER_NAME}'@'%';" >> $temp_file
+			echo "CREATE USER '${DB_USER_NAME}'@'localhost' IDENTIFIED BY '${DB_USER_PASSWORD}';" >> $temp_file
+			echo "GRANT ALL ON \`${DB_TITLE}\`.* to '${DB_USER_NAME}'@'localhost';" >> $temp_file
 		else
 			echo "[i] User '${DB_USER_NAME}' already exists in database."
 		fi
 	fi
 
-	#
 	/usr/bin/mysqld --user=mysql --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0 < $temp_file
 	rm -f $temp_file
 
