48e77219f0b8e19c8d9b358852d7f01422b4e592
nplieger <plieger05@gmail.com>
2023-09-24 15:44:24 +0200

    Add port to wp-config

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', getenv('DB_USER_NAME') ?: 'root' );
 define( 'DB_PASSWORD', getenv('DB_USER_PASSWORD') ?: 'password' );
 
 /** Database hostname */
-define( 'DB_HOST', getenv('DB_HOST') ?: 'localhost' );
+define( 'DB_HOST', getenv('DB_HOST') . ':9000' ?: 'localhost:9000' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
