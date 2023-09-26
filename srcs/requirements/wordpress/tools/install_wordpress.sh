7caa563963e9e307dd81f49165845dcc44e6b07d
nplieger <plieger05@gmail.com>
2023-09-26 11:59:01 +0200

    Add sed

--- srcs/requirements/wordpress/tools/install_wordpress.sh
+++ srcs/requirements/wordpress/tools/install_wordpress.sh
@@ -8,19 +8,25 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 	tar -xzvf wordpress.tar.gz -C /var/www/html/ --strip-components=1;
 	rm wordpress.tar.gz;
 
-	echo "[i] Generating /var/www/html/wp-config.php file"
+	echo "[i] Adding sed."
+	apk add --no-cache sed
+	echo "[i] Generating /var/www/html/wp-config.php file."
 	cat /tmp/wp-config.php.template |
 		sed -e "s#\${DB_TITLE}#${DB_TITLE}#g" \
 	  		-e "s#\${DB_USER_NAME}#${DB_USER_NAME}#g" \
 	  		-e "s#\${DB_USER_PASSWORD}#${DB_USER_PASSWORD}#g" \
 	  		-e "s#\${DB_HOST}#${DB_HOST}#g" \
 		> /var/www/html/wp-config.php
-	rm -f /etc/wp-config.php.template
+
 	chown www-data:www-data /var/www/html/wp-config.php
+
+	echo "[i] Remove sed and template."
+	rm -f /etc/wp-config.php.template
 
 else
 
 	echo "[i] Wordpress already downloaded"
+	echo "[i] Remove sed and template."
 	rm -rf /tmp/wp-config.php.template
 
 fi
