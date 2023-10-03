4e515326c42a3dffa39787a10a61f0cd42d02209
nplieger <plieger05@gmail.com>
2023-10-03 17:46:34 +0200

    Add primary setup of FTP

---
+++ srcs/requirements/bonus/ftp/tools/configure_vsftpd.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+echo "[i] Append conf.d inclusion to vsftpd.conf file."
+cat <<EOL >> /etc/vsftpd/vsftpd.conf
+
+# Include all configuration files from the conf.d directory
+include=/etc/vsftpd/conf.d/*.conf
+EOL
+
+echo "[i] Create FTP user in non-interactive login mode."
+adduser ${FTP_USER_NAME} -s /bin/false ${FT_USER_NAME}
+
+echo "[i] Set the FTP user's password."
+echo "${FTP_USER_NAME}:${FTP_USER_PASSWORD}" | chpasswd
+
+echo "[i] Create /var/www/html if not already existant in volume and give permissions to FTP user."
+mkdir -p /var/www/html
+chown -R ${FTP_USER_NAME} /var/www/html
+
+echo "[i] Create vsftpd.userlist and add our FTP user to it."
+echo ${FTP_USR_NAME} > /etc/vsftpd.userlist &> /dev/null
+
+exec "$@"
--- srcs/requirements/bonus/redis/tools/configure_redis.sh
+++ srcs/requirements/bonus/redis/tools/configure_redis.sh
@@ -3,8 +3,9 @@
 echo "[i] Move redis.conf to /etc/redis/ directory"
 mv /etc/redis.conf /etc/redis/
 
-echo "[i] Append conf.d inclusion for redis.conf file."
+echo "[i] Append conf.d inclusion to redis.conf file."
 cat <<EOL >> /etc/redis/redis.conf
+
 # Include all configuration files from the conf.d directory
 include /etc/redis/conf.d/*.conf
 EOL
